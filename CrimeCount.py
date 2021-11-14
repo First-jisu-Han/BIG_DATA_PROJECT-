fbedfe477da05b6a53f6fafecd115d04307768d8
Jisu Han <weblite@naver.com>
2021-11-14 23:14:04 +0900

    주제 명명

--- CrimeCount.py
+++ CrimeCount.py
@@ -2,6 +2,7 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 
+# 지역별 범죄 발생 빈도와 종류를 통한 치안 관리 효율화 
 
 API_Key= "[jGRnEP%2FmMvJx1xoDf7VVRaEKZHYagcn%2BiAgUGpY4SqyT8diNLCkfHgqOfsbYNXpoRw3XnwQQQwdO23DO2ju8Og%3D%3D]"
 URL= "https://api.odcloud.kr/api/3074462/v1/uddi:10bde8f1-739c-4b66-b6a6-ccf5339a658e_201910221521?page=1&perPage=10"
